52bdfe03337b217206d0841a751b213ac4689b60
PabloAvelar <pabloarmenta84@gmail.com>
2025-01-24 16:51:59 -0600

    fix vite.config.js env variables

--- vite.config.js
+++ vite.config.js
@@ -13,10 +13,10 @@ export default defineConfig({
     ],
 
     server: {
-        host: process.env.VITECONF_LOCAL || '0.0.0.0',  // Permite conexiones externas
-        port: process.env.VITECONF_HOST || 5173,       // Define el puerto (puede ser cualquier puerto disponible)
+        host: process.env.VITECONF_HOST || '0.0.0.0',  // Permite conexiones externas
+        port: process.env.VITECONF_PORT || 5173,       // Define el puerto (puede ser cualquier puerto disponible)
         hmr: {
-            host: process.env.VITECONF_PORT || 'blindspot.media.mit.edu',  // Reemplaza con la IP de tu servidor local
+            host: process.env.VITECONF_LOCAL || 'blindspot.media.mit.edu',  // Reemplaza con la IP de tu servidor local
         },
     },
 });
