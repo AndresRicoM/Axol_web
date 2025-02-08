5136c08ab852824d4a4e047682b92e8f2c12eb71
PabloAvelar <pabloarmenta84@gmail.com>
2025-02-08 15:58:43 -0600

    fix: flex-row cs logo

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -1,6 +1,6 @@
 export default function ApplicationLogo(props) {
     return (
-        <div>
+        <div className="flex flex-row gap-2">
             <img {...props} src="/assets/AxolSitting.png" alt="Mi Logo" />
             <img {...props} src="/assets/logosblack.png" alt="MIT City Science" />
         </div>
